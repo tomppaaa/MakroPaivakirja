cb9ccb0a458e4c5c2d26c3c3f084e4ab22998dbd
tomppaaa <thomas.tran@helsinki.fi>
2026-09-14 15:13:48 +0300

    testi

--- app.py
+++ app.py
@@ -13,3 +13,14 @@ def page1():
 @app.route("/page2")
 def page2():
     return "Toinen sivu"
+
+@app.route("/test")
+def test():
+    content = ""
+    for i in range(1,101):
+        content += str(i) + " "
+    return content
+
+@app.route("/page/int:page_id>")
+def page(page_id):
+    return "Tämä on sivu " + str(page_id) 
